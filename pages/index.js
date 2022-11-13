84721ddbfb6c7ea859a97e6c9832b6ef0876625e
Rafiul <ahmadrafiulm@gmail.com>
2022-11-13 08:49:00 +0700

    meta tagss

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,12 @@ export default function Home() {
     <div className='font-archivo font-normal bg-white dark:bg-black dark:text-white'>
       <Head>
         <title>Ahmad Rafiul Mahdi</title>
-        <meta name="description" content="Ahmad Rafiul Mahdi - Web Developer, UI/UX Designer, Graphic Designer" />
+        <meta name="description" content="Web Developer, UI/UX Designer, Graphic Designer. Bachelor of Engineering with a GPA of 3.48 majoring in Computer Engineering from the University of Indonesia. More than 2 years of work experience as a designer and developer. Very high english profiency (IELTS Band 8.0)" />
+        <meta property='og:title' content='Ahmad Rafiul Mahdi' />
+        <meta property='og:description' content='Web Developer, UI/UX Designer, Graphic Designer. Bachelor of Engineering with a GPA of 3.48 majoring in Computer Engineering from the University of Indonesia. More than 2 years of work experience as a designer and developer. Very high english profiency (IELTS Band 8.0)' />
+        <meta property='og:image' content='/iul-logo.svg' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://www.rafiulm.com/' />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 

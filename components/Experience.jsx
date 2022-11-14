d6283d253018590ed77007ee2332d4cc2da67fdf
Rafiul <ahmadrafiulm@gmail.com>
2022-11-14 21:13:03 +0700

    key prop experience

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -11,7 +11,7 @@ function Experience() {
       <div className="flex overflow-x-scroll md:overflow-visible scrollbar-hide gap-4  px-4 md:px-0 py-4">
         {ExperienceData.map((data) => {
           return (
-            <div className="flex flex-shrink-0 md:flex-shrink rounded-lg bg-white dark:bg-black dark:outline-zinc-900 dark:outline shadow-lg w-[320px] flex-col px-4 py-6 hover:scale-[1.03] duration-500 ease-in-out">
+            <div key={data.id} className="flex flex-shrink-0 md:flex-shrink rounded-lg bg-white dark:bg-black dark:outline-zinc-900 dark:outline shadow-lg w-[320px] flex-col px-4 py-6 hover:scale-[1.03] duration-500 ease-in-out">
               <div className="flex flex-col">
                 <span className="italic text-sm">{data.year}</span>
                 <span className="flex flex-col w-full font-bold text-lg">
